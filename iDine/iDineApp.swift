defed9f59eec061c57595b4206d485deb4be12ab
VolodymyrDziu <credo-planers-0i@icloud.com>
2025-04-09 16:39:58 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Volodymyr Dziubenko on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
